3091854726fdc4f0d7b31ac03d2e8c41ced3b9ef
Ritvik Soni <86793647+Ritviksoni16@users.noreply.github.com>
2023-07-12 15:11:56 +0530

    Add files via upload
    Following are the files to make the project work

---
+++ AviateDashboard.js
@@ -0,0 +1,304 @@
+import "./AviateDashboard.css";
+const AviateDashboard = () => {
+  return (
+    <div className="aviate-dashboard">
+      <div className="top-nav-bar">
+        <div className="top-nav-bar-child" />
+        <img className="group-icon" alt="" src="/group.svg" />
+        <img className="rewards-icon" alt="" src="/rewards.svg" />
+        <div className="top-nav-bar-item" />
+        <div className="top-nav-bar-inner" />
+        <img className="award-icon" alt="" src="/award.svg" />
+        <img className="messages-icon" alt="" src="/messagesicon.svg" />
+        <div className="ellipse-div" />
+        <div className="top-nav-bar-child1" />
+        <div className="div">8</div>
+        <div className="div1">3</div>
+        <div className="aviate-logo">
+          <img className="aviate-logo1" alt="" src="/aviate-logo@2x.png" />
+        </div>
+        <div className="about-me">
+          <img className="about-me-child" alt="" src="/ellipse-35@2x.png" />
+          <div className="edit-profile-button">
+            <div className="edit-profile-button-child" />
+            <div className="edit-my-profile">EDIT MY PROFILE</div>
+          </div>
+          <div className="akash-mishra">Akash Mishra</div>
+        </div>
+        <div className="search-bar-top">
+          <div className="search-bar-top-child" />
+          <div className="type-for-search">Type for Search</div>
+          <img className="group-icon1" alt="" src="/group1.svg" />
+        </div>
+      </div>
+      <div className="dashboard">Dashboard</div>
+      <div className="jobs-recommendations-for-container">
+        <span className="jobs-recommendations">{`Jobs Recommendations `}</span>
+        for You
+      </div>
+      <div className="see-all">
+        <span className="jobs-recommendations">{`See `}</span>all
+      </div>
+      <div className="job-recomm-1">
+        <div className="ux-designer">UX Designer</div>
+        <div className="ux-designer1">UX Designer</div>
+        <div className="avalon-meta">{`Avalon Meta , `}</div>
+        <div className="cred">CRED ,</div>
+        <div className="weeks-ago">3 Weeks ago</div>
+        <div className="week-ago">1 week ago</div>
+        <div className="applicants">100 Applicants</div>
+        <div className="applicants1">12 Applicants</div>
+        <div className="apply-now-button">
+          <div className="rectangle-parent">
+            <div className="group-child" />
+            <div className="group-item" />
+          </div>
+          <div className="apply-now">{`Apply Now `}</div>
+        </div>
+        <div className="apply-now-button1">
+          <div className="apply-now">{`Apply Now `}</div>
+        </div>
+        <div className="job-recomm-1-child" />
+        <div className="job-recomm-1-item" />
+        <img className="job-recomm-1-inner" alt="" src="/frame-24.svg" />
+        <div className="mumbai">Mumbai</div>
+        <div className="bengaluru">Bengaluru</div>
+        <div className="job-recomm-1-child1" />
+        <div className="job-recomm-1-child2" />
+        <img className="onsite-icon" alt="" src="/onsite.svg" />
+        <img className="group-icon2" alt="" src="/group2.svg" />
+        <img className="group-icon3" alt="" src="/group3.svg" />
+      </div>
+      <img className="dots-icon" alt="" src="/3-dots.svg" />
+      <img className="dots-icon1" alt="" src="/3-dots1.svg" />
+      <div className="aviate-dashboard-child" />
+      <div className="aviate-dashboard-item" />
+      <div className="aviate-dashboard-inner" />
+      <div className="ux-designer2">UX Designer</div>
+      <div className="mumbai-bengaluru">Mumbai , Bengaluru , pune</div>
+      <div className="salary-range">
+        <div className="div2">₹</div>
+        <div className="div3">₹</div>
+        <div className="k-75k">50K - 75K</div>
+      </div>
+      <div className="upcoming-events">Upcoming Events</div>
+      <div className="rectangle-div" />
+      <div className="job-search-status">Job search status</div>
+      <div className="actively-interviewing">Actively Interviewing</div>
+      <img className="vector-icon" alt="" src="/vector.svg" />
+      <div className="aviate-dashboard-child1" />
+      <div className="jd-1">
+        <div className="jd-1-child" />
+        <div className="jd-1-item" />
+        <div className="k-container">
+          <span className="span">₹</span>
+          <span className="k-">{` 50K - `}</span>
+          <span className="span">{`₹ `}</span>
+          <span className="k-">{`70K   `}</span>
+        </div>
+        <div className="per-month">Per Month</div>
+        <div className="jd-1-inner" />
+        <div className="chances">70% Chances</div>
+        <div className="jd-1-child1" />
+        <img className="rectangle-icon" alt="" src="/rectangle-293@2x.png" />
+        <img className="jd-1-child2" alt="" src="/rectangle-296@2x.png" />
+        <img className="jd-1-child3" alt="" src="/rectangle-298@2x.png" />
+        <div className="senior-product-designer">Senior Product Designer</div>
+        <div className="senior-product-designer1">Senior Product Designer</div>
+        <div className="senior-product-designer2">Senior Product Designer</div>
+        <div className="techmint-on-site">{`Techmint , On-site   `}</div>
+        <div className="unacademy">{`Unacademy  ,    `}</div>
+        <div className="work-from-home">work from home</div>
+        <div className="udemy-on">Udemy , on site</div>
+        <div className="bengaluru-india">Bengaluru, India</div>
+        <div className="bengaluru-india1">Bengaluru, India</div>
+        <div className="bengaluru-india2">Bengaluru, India</div>
+        <div className="rejected">
+          <div className="rejected-child" />
+          <div className="rejected1">Rejected</div>
+        </div>
+        <div className="in-process">
+          <div className="in-process-child" />
+          <div className="in-process1">In Process</div>
+        </div>
+        <div className="selected">
+          <div className="selected-child" />
+          <div className="selected1">Selected</div>
+        </div>
+        <div className="see-all1">
+          <span className="jobs-recommendations">{`See `}</span>all
+        </div>
+        <div className="applications">Applications</div>
+        <div className="k-container1">
+          <span className="span">₹</span>
+          <span className="k-">{` 60K - `}</span>
+          <span className="span">₹ 8</span>
+          <span className="k-">{`0K   `}</span>
+        </div>
+        <div className="k-container2">
+          <span className="span">₹</span>
+          <span className="k-">{` 60K - `}</span>
+          <span className="span">₹ 75</span>
+          <span className="k-">{`K   `}</span>
+        </div>
+        <div className="per-month1">Per Month</div>
+        <div className="per-month2">Per Month</div>
+        <img className="component-15-icon" alt="" src="/component-15.svg" />
+      </div>
+      <div className="home">
+        <div className="home-child" />
+        <div className="log-out">Log out</div>
+        <div className="help-button">
+          <div className="help-button-child" />
+          <div className="help">HELP</div>
+        </div>
+        <img className="hamburger-icon" alt="" src="/hamburger.svg" />
+        <img className="logout-icon" alt="" src="/logouticon.svg" />
+        <div className="woooo">
+          <div className="component-2">
+            <div className="home1">
+              <div className="dashboard1">Dashboard</div>
+            </div>
+            <div className="rectangle-group">
+              <div className="group-inner" />
+              <div className="group-child1" />
+            </div>
+            <div className="component-7dashboard">
+              <div className="component-7dashboard-child" />
+              <div className="component-7dashboard-item" />
+              <div className="component-7dashboard-inner" />
+              <div className="component-7dashboard-child1" />
+            </div>
+          </div>
+          <div className="component-4">
+            <div className="messages">
+              <div className="messages1">Messages</div>
+              <img className="messages-icon1" alt="" src="/messagesicon1.svg" />
+            </div>
+          </div>
+          <div className="component-7">
+            <div className="yellow-selected" />
+            <div className="jobs-icon">
+              <img className="vector-icon1" alt="" src="/vector1.svg" />
+              <img className="vector-icon2" alt="" src="/vector2.svg" />
+              <div className="my-jobs">My Jobs</div>
+            </div>
+          </div>
+          <div className="component-6">
+            <div className="up-com-events">
+              <div className="upcoming-events-icon-parent">
+                <img
+                  className="upcoming-events-icon"
+                  alt=""
+                  src="/upcoming-eventsicon.svg"
+                />
+                <div className="group-child2" />
+                <div className="div4">3</div>
+              </div>
+              <div className="upcoming-events1">
+                <p className="upcoming">upcoming</p>
+                <p className="upcoming">events</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="event-reminder-rect-parent">
+        <div className="event-reminder-rect" />
+        <img className="group-icon4" alt="" src="/group4.svg" />
+        <div className="mock-interview">Mock Interview</div>
+        <div className="portfolio-presentation">Portfolio Presentation</div>
+        <div className="technical-round">Technical Round</div>
+        <div className="am-1100">(9:00 AM - 11:00 AM)</div>
+        <div className="am-200">(12:00 AM - 2:00 PM)</div>
+        <div className="am-2001">(12:00 AM - 2:00 PM)</div>
+        <div className="th-august">24th August , 2022</div>
+        <div className="th-sept">10th Sept , 2022</div>
+        <div className="th-sept1">10th Sept , 2022</div>
+        <div className="frame-child" />
+        <div className="frame-item" />
+        <div className="frame-inner" />
+        <div className="enroll-now">Enroll Now</div>
+        <div className="enroll-now1">Enroll Now</div>
+        <div className="enroll-now2">Enroll Now</div>
+        <div className="line-div" />
+        <div className="frame-child1" />
+        <div className="frame-child2" />
+        <div className="see-all2">
+          <span className="jobs-recommendations">{`See `}</span>all
+        </div>
+        <img className="dots-icon2" alt="" src="/3-dots2.svg" />
+        <div className="image-40-parent">
+          <img className="image-40-icon" alt="" src="/image-40@2x.png" />
+          <div className="group-child3" />
+        </div>
+        <div className="suggested-sessions">Suggested Sessions</div>
+      </div>
+      <div className="event-reminder-rect-group">
+        <div className="event-reminder-rect1" />
+        <div className="event-reminder1">
+          <div className="event-reminder1-child" />
+          <div className="pesto-parent">
+            <div className="pesto">Pesto</div>
+            <img className="group-child4" alt="" src="/rectangle-302@2x.png" />
+          </div>
+          <div className="event-reminder1-item" />
+          <div className="am-1100-container">
+            <span className="jobs-recommendations">{`9:00 `}</span>AM - 11:00 AM
+          </div>
+          <div className="tomorrow">Tomorrow</div>
+          <div className="portfolio-presentation1">Portfolio Presentation</div>
+          <div className="event-reminder1-inner" />
+        </div>
+        <div className="event-reminder11">
+          <div className="event-reminder1-child1" />
+          <div className="event-reminder1-child2" />
+          <div className="event-reminder1-child3" />
+          <div className="am-1000-container">
+            <span className="jobs-recommendations">{`9:00 `}</span>AM - 10:00 AM
+          </div>
+          <div className="th-august1">25th August , 2022</div>
+          <div className="group-discussion">Group Discussion</div>
+          <div className="vernacularai-wrapper">
+            <div className="vernacularai">vernacular.ai</div>
+          </div>
+          <div className="rectangle-container">
+            <div className="group-child5" />
+            <img className="group-child6" alt="" src="/rectangle-303@2x.png" />
+            <img className="group-child7" alt="" src="/rectangle-3021@2x.png" />
+          </div>
+        </div>
+        <div className="event-reminder12">
+          <div className="event-reminder1-child4" />
+          <div className="event-reminder1-child5" />
+          <div className="groww-wrapper">
+            <div className="groww">Groww</div>
+          </div>
+          <div className="event-reminder1-child6" />
+          <div className="event-reminder1-child7" />
+          <div className="pm-400-container">
+            <span className="jobs-recommendations">2:00 P</span>M - 4:00 PM
+          </div>
+          <div className="th-august2">29th August , 2022</div>
+          <div className="mock-interview-session">Mock Interview Session 1</div>
+          <img
+            className="event-reminder1-child8"
+            alt=""
+            src="/rectangle-311@2x.png"
+          />
+          <img
+            className="event-reminder1-child9"
+            alt=""
+            src="/rectangle-312@2x.png"
+          />
+        </div>
+        <img className="group-icon5" alt="" src="/group5.svg" />
+        <div className="frame-child3" />
+        <div className="august-2022">August , 2022</div>
+        <img className="vector-icon3" alt="" src="/vector3.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default AviateDashboard;
